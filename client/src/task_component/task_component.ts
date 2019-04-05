303102b0f90fa193cbc5758f2de08f996b8bc5fd
Kozo Oeda <kozo.oeda@gmail.com>
2019-04-05 22:17:57 +0900

    Change tab spaces

--- client/src/task_component/task_component.ts
+++ client/src/task_component/task_component.ts
@@ -4,22 +4,19 @@ import { Collection } from "./task_collection"
 
 
 export class Atom extends Core {
-    constructor(init: Init, parent_id: ParentID = "", public todo: string = "", public attack: string = "") {
-        super(init, parent_id, "atom")
-    }
-
+  constructor(init: Init, parent_id: ParentID = "", public todo: string = "", public attack: string = "") {
+    super(init, parent_id, "atom")
+  }
 }
 
 export class Molecule extends Collection < Atom > {
-    constructor(init: Init, parent_id: ParentID = "", public abstract_text: string = "", public attack: string = "", time_plan: TimePlan) {
-        super(init, parent_id, "molecule", time_plan);
-    }
-
+  constructor(init: Init, parent_id: ParentID = "", public abstract_text: string = "", public attack: string = "", time_plan: TimePlan) {
+    super(init, parent_id, "molecule", time_plan);
+  }
 }
 
 export class Beaker extends Collection < Molecule > {
-    constructor(init: Init, parent_id: ParentID = "", public title: string = "", public note: string = "", time_plan: TimePlan) {
-        super(init, parent_id, "beaker", time_plan);
-    }
-
+  constructor(init: Init, parent_id: ParentID = "", public title: string = "", public note: string = "", time_plan: TimePlan) {
+    super(init, parent_id, "beaker", time_plan);
+  }
 }
