58e1139c0654f4d99c0f68e6aea6f7e5609be654
Kozo Oeda <kozo.oeda@gmail.com>
2019-04-05 21:34:13 +0900

    Update

--- client/src/task_component/tmp.ts
+++ client/src/task_component/tmp.ts
@@ -1,35 +1,39 @@
-import {Atom, Molecule, Beaker} from "./task_component"
+import {
+  Atom,
+  Molecule,
+  Beaker
+} from "./task_component"
 
 let a = new Atom({
-    is_first: true,
-    user_id: ""
+  is_first: true,
+  user_id: ""
 }, "atom_test_id");
 
 console.log("level");
 console.log(a.level(5, true));
 
 let m = new Molecule({
-        is_first: true,
-        user_id: ""
-    }, "molecule_test_id",
-    "test_abstract",
-    "test_attack", {
-        whole_time: 60,
-        buffer: 30
-    }
+    is_first: true,
+    user_id: ""
+  }, "molecule_test_id",
+  "test_abstract",
+  "test_attack", {
+    whole_time: 60,
+    buffer: 30
+  }
 );
 
 m.add(a)
 
 let b = new Beaker({
-        is_first: true,
-        user_id: ""
-    }, "beaker_test_id",
-    "test_title",
-    "test_note", {
-        whole_time: 60,
-        buffer: 30
-    }
+    is_first: true,
+    user_id: ""
+  }, "beaker_test_id",
+  "test_title",
+  "test_note", {
+    whole_time: 60,
+    buffer: 30
+  }
 );
 
 b.add(m);
